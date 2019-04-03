4806021a42be4847cb6ee6f930ce2da488655760
soniashvin <35840223+soniashvin@users.noreply.github.com>
2019-04-03 22:54:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,29 +20,16 @@ cluster_name='cluster-e0a5'
 bucket_name=''
 filename='train_and_apply.py'
 bucket_name='my-bitnami-project-236405'
-def submit_pyspark_job(dataproc, project, region,
-                       cluster_name, bucket_name, filename):
+
+def submit_pyspark_job(dataproc, project, region, cluster_name, bucket_name, filename):
     """Submits the Pyspark job to the cluster, assuming `filename` has
     already been uploaded to `bucket_name`"""
-    job_details = {
-        'projectId': project,
-        'job': {
-            'placement': {
-                'clusterName': cluster_name
-            },
-            'pysparkJob': {
-                'mainPythonFileUri':'gs://{}/{}'.format(bucket_name, filename)
-            }
-        }
-    }
-    result = dataproc.projects().regions().jobs().submit(
-        projectId=project,
-        region=region,
-        body=job_details).execute()
+    job_details = {   'projectId': project,   'job': {     'placement': {          'clusterName': cluster_name       },      'pysparkJob': {   'mainPythonFileUri':'gs://{}/{}'.format(bucket_name, filename)            }     }    }
+	
+    result = dataproc.projects().regions().jobs().submit(projectId=project,region=region,body=job_details).execute()
     job_id = result['reference']['jobId']
     print('Submitted job ID {}'.format(job_id))
     return job_id
-
 
 # [START form]
 @app.route('/form')
